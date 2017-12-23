51397d1130d7a471bcd8d125d56eb8b98f4e2a04
Arshia <arshiakohanteb@gmail.com>
2017-12-22 17:21:44 -0800

    Finished Buble Sort/ Swap Sort

--- Bubble Sorting/Program.cs
+++ Bubble Sorting/Program.cs
@@ -10,7 +10,45 @@ namespace Bubble_Sorting
     {
         static void Main(string[] args)
         {
+            List<int> ints = new List<int>();
+            Random rand = new Random();
+            
+            for (int i = 0; i < 20; i++)
+            {
+                ints.Add(rand.Next(1,11));
+            }
+            foreach (int num in ints)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("");
+            BubbleSort(ints);
+            foreach (int num in ints)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+        static void BubbleSort(List<int> numbers)
+        {
+            bool Swapping;
+            do
+            {
 
+                Swapping = false;
+                for (int i = 1; i < numbers.Count; i++)
+                {
+                    if (numbers[i - 1] > numbers[i])
+                    {
+                        Swapping = true;
+                        int temp;
+                        temp = numbers[i];
+                        numbers[i] = numbers[i - 1];
+                        numbers[i - 1] = temp;
+
+                    }
+                }
+            } while (Swapping);
         }
     }
 }
